f6fedbd0df00d0d279c64409786b4dcfe4461dae
Zach Cleveland <zachcleveland@gmail.com>
2023-03-08 15:19:51 -0700

    adding sound
    initial attempt at sfx.

--- player.lua
+++ player.lua
@@ -7,10 +7,14 @@ end
 
 function playerkeypress()
     -- Key Presses
-	if btn(0) then p.x-=1 end -- Left Arrow
+	if btn(0) then p.x-=2 end -- Left Arrow
  	if btn(1) then p.x+=1 end -- Right Arrow
- 	if btn(2) then p.y-=1 end -- Up Arrow
+ 	if btn(2) then 
+        p.y-=1 end -- Up Arrow
  	if btn(3) then p.y+=1 end -- Down Arrow 
+    if btn(2) then
+        sfx(5, -1, 0, 31)
+        end
 end
 
 function player_draw()
