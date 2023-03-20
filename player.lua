a819d95f42d119baae5b4b4ff9d33b5f5549db53
JaysonRusJ <98131260+JaysonRusJ@users.noreply.github.com>
2023-03-20 09:45:49 -0600

    v0.3
    collision in bedroom should work
    you can jump to a playtest by moving to a monitor in the bedroom and pressing 'x'

--- player.lua
+++ player.lua
@@ -1,71 +1,292 @@
-// players inital values
+
 function init_player()
-    p={}          -- player's table
-    p.x=10        -- player's X
-    p.y=10        -- player's y
-    p.state=1     -- player's animation state
-    p.dirt=0      -- player's direction 
-    p.defstate=1  -- player's default state
-    p.anim_time=0 -- player's animation time
+    -- player info
+    p = {
+      -- sprite number
+      sp = 1,
+
+      -- location
+      x = 59,
+      y = 59,
+
+      -- mesaure width and height in pixels
+      w = 8,
+      h= 8,
+
+      -- direction player is facing
+      flipx = false,
+
+      -- change in x and y
+      dx = 0,
+      dy = 0,
+
+      -- maximum change in x and y
+      max_dx = 2,
+      max_dy = 3,
+
+      -- velocity for running and jumping
+      acceleration = 0.5,
+      boost = 4,
+
+      animation = 0,
+
+      running = false,
+      jumping = false,
+      sliding = false,
+      landed = false,
+      falling = false
+
+  }
+
 end
 
-// updates the player
-function player_update()
-    -- intit value
-	move = -1
+-- playtest controls
+function player_playtest()
+  p.dy += gravity
+  p.dx *= friction
 
-    -- Check for button inputs
-    -- Left Arrow
-    if btn(0) then 
-        p.x-=1
-        move = 15 
-        p.defstate=16
-    end 
+  -- move left
+  if btn(0) then
+      p.dx -= p.acceleration
+      p.running = true
+      p.flipx = true
+  end
 
-    -- Right Arrow
- 	if btn(1) then 
-        p.x+=1 
-        move = 0 
-        p.defstate=1
-    end 
+  -- mode right
+  if btn(1) then
+      p.dx += p.acceleration
+      p.running = true
+      p.flipx = false
+  end
 
-    -- Up Arrow
- 	if btn(2) then 
-        p.y-=1 
-        move = 47 
-        p.defstate=48
+  -- slide
+  if p.running
+  and not btn(0)
+  and not btn(1)
+  and not p.falling
+  and not p.jumping then
+      p.running = false
+      p.sliding = true
+  end
+
+  -- jump
+  if btnp(2)
+  and p.landed then
+      p.dy -= p.boost
+      p.landed = false
+  end
+
+  -- check collision up and down
+  if p.dy > 0 then
+      p.falling = true
+      p.landed = false
+      p.jumping = false
+
+      p.dy = limit_speed(p.dy,p.max_dy)
+
+      if collide_map(p,"down",0) then
+          p.landed = true
+          p.falling = false
+          p.dy = 0
+          p.y -= ((p.y+p.h+1)%8)-1
+      end
+
+  elseif p.dy < 0 then
+      p.jumping = true
+      if collide_map(p,"up",1) then
+          p.dy = 0
+      end
+
+  end
+
+  -- check collision left
+  if p.dx < 0 then
+
+    p.dx = limit_speed(p.dx,p.max_dx)
+
+    if collide_map(p,"left",1) then
+      p.dx = 0
     end
-    
-    -- Down Arrow
- 	if btn(3) then 
-        p.y+=1 
-        move = 31
-        p.defstate=32
-    end 
 
-    -- return if no input detected
-    if move == -1 then return end
+  -- check collision right
+  elseif p.dx > 0 then
 
-    -- Check if appropriate time to update animation
-    if time() - p.anim_time <= 0.1 then return end
-    -- update the direction the player
-    -- is facing
-    if p.dirt != move then
-        p.dirt = move
-        p.state = move
+    p.dx = limit_speed(p.dx,p.max_dx)
+
+    if collide_map(p,"right",1) then
+      p.dx = 0
     end
-    -- update the state
-    p.state +=1
 
-    p.anim_time = time()
+  end
 
-    -- check if its the last state
-    if p.state > 3 + p.defstate then
-        p.state = p.defstate
+  -- stop sliding
+  if p.sliding then
+    if abs(p.dx) < .2
+      or p.running then 
+        p.dx = 0
+        p.sliding = false
     end
+  end
+
+  -- update player's position
+  p.x += p.dx
+  p.y += p.dy
+
+  -- limit player to map
+  if p.x < map_start then
+      p.x = map_start
+  end
+
+  if p.x > map_end - p.w then
+      p.x = map_end - p.w
+  end
+
+  -- teleport back to bedroom
+  if (p.x >= 456 and p.x <= 512 ) then
+    teleport(0)
+  elseif (p.x >= 840) then
+    teleport(0)
+  end
+
+  -- player falls out of the world
+  if (p.y >= 128 and p.x <= 520) then
+    teleport(1)
+  elseif (p.y >= 136 and p.x >= 520) then
+    teleport (2)
+  end
+
+
+
+end
+
+-- collsion between player and map
+function collide_map(obj,aim,flag)
+    --obj = table needs x,y,w,h
+    --aim = left,right,up,down
+   
+    local x=obj.x  
+    local y=obj.y
+    local w=obj.w  
+    local h=obj.h
+   
+    local x1=0	 
+    local y1=0
+    local x2=0  
+    local y2=0
+   
+    if aim=="left" then
+      x1=x-1  
+      y1=y
+      x2=x    
+      y2=y+h-1
+   
+    elseif aim=="right" then
+      x1=x+w-1    
+      y1=y
+      x2=x+w  
+      y2=y+h-1
+   
+    elseif aim=="up" then
+      x1=x+2    
+      y1=y-1
+      x2=x+w-3  
+      y2=y
+   
+    elseif aim=="down" then
+      x1=x+2     
+      y1=y+h
+      x2=x+w-3    
+      y2=y+h
+    end
+
+    if scene == 0 then
+      if aim == "left" then
+        x1=x + 3
+        y1=y + (h/2)
+        x2=x + 3   
+        y2=y+h-1
+
+      elseif aim=="right" then
+        x1=x+w-4    
+        y1=y + (h/2)
+        x2=x+w-4 
+        y2=y+h-1
+
+      elseif aim=="up" then
+        x1=x+4    
+        y1=y
+        x2=x+w-5  
+        y2=y
+     
+      elseif aim=="down" then
+        x1=x + 4    
+        y1=y+h
+        x2=x+w - 5    
+        y2=y+h
+      end
+    end
+
+    xr1 = x1
+    xr2 = x2
+    yr1 = y1
+    yr2 = y2
+   
+    --pixels to tiles
+    x1/=8    
+    y1/=8
+    x2/=8    
+    y2/=8
+   
+    if fget(mget(x1,y1), flag)
+    or fget(mget(x1,y2), flag)
+    or fget(mget(x2,y1), flag)
+    or fget(mget(x2,y2), flag) then
+      return true
+    else
+      return false
+    end
+   
+end
+
+function player_animate()
+
+  if p.jumping then
+    p.sp = 8
+
+  elseif p.falling then
+    p.sp = 9
+
+  elseif p.sliding then
+    p.sp = 10
+
+  elseif p.running then
+    -- loop through sprites based
+    if time() - p.animation > 0.1 then
+      p.animation = time()
+      p.sp +=1
+      if p.sp > 7 then
+        p.sp = 4
+      end
+    end
+
+  else
+    -- loop through sprites
+    if time() - p.animation > 0.3 then
+      p.animation = time()
+      p.sp += 1
+      if p.sp > 3 then
+        p.sp = 2
+      end
+     end
+  end
+
+end
+
+-- controls player speed
+function limit_speed(num,maximum)
+  return mid(-maximum,num,maximum)
 end
 
 function player_draw()
-    -- draw sprite
-    spr(p.state, p.x, p.y)
+    spr(1, p.x, p.y)
 end
