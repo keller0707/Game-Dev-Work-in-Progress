30b947094004b3f9898bbfcb0e90cce4cf5cf588
Keller Sedillo <kellersedillo@hotmail.com>
2023-04-26 14:57:53 -0600

    Added new shapes and rotation to mini-game

--- collision/block.lua
+++ collision/block.lua
@@ -1,5 +1,5 @@
-function add_block(block_x, block_y, block_color)
-    add(blocks, {
+function add_block(list, block_x, block_y, block_color)
+    add(list, {
         -- Set Position
         x = block_x,
         y = block_y,
@@ -7,61 +7,12 @@ function add_block(block_x, block_y, block_color)
         -- Set Color
         color = block_color,
 
-        -- Block Speed
-        speed = 8,
+        update_x = function(self, set_speed)
+            self.x += set_speed
+        end,
 
-        -- Clock 
-        clock = 0,
-
-        -- Key Press
-        pressed = false,
-
-        -- Update Function
-        update = function(self)
-            -- Get current Cell Bellow
-            cellx = (self.x/8)
-            celly = (self.y/8)
-
-            -- Get Sprites around the block
-            cellsprlft = mget(cellx-1, celly)
-            cellsprrht = mget(cellx+1, celly)
-
-            -- Create togle
-            if not self.pressed then
-                -- Left Arrow Input
-                if btn(0) then
-                    -- Check for walls/blocks
-                    if not fget(cellsprlft, 0) then
-                        -- Update Position
-                        self.x -= self.speed
-                    end  
-                end
-
-                -- Right Arrow Input
-                if btn(1) then
-                    -- Check for walls/blocks
-                    if not fget(cellsprrht, 0) then
-                        -- Update Position
-                        self.x += self.speed
-                    end
-                end
-
-                -- Update Press
-                self.pressed = true
-            else
-                -- Update Press
-                self.pressed = false
-            end
-
-            -- Check if time to update
-            if time() - self.clock <= 1 then return end
-            
-           
-            -- Update y position
-            self.y += self.speed
-
-            -- Update clock
-            self.clock = time()
+        update_y = function(self, set_speed)
+            self.y += set_speed
         end,
 
         -- Draw Function
@@ -72,25 +23,29 @@ function add_block(block_x, block_y, block_color)
 
         
         -- Kill Function
-        kill = function(self)
+        kill = function(self, list)
             -- Draw obj on map
             mset(self.x/ 8, self.y /8, self.color)
-            
-            -- Remove From List
-            del(blocks, self)
+
+            -- Remove from list
+            del(list, self)
         end,
 
-        -- Collision Check
-        check_collision = function(self)
-            -- Get current Cell Bellow
-            cellx = (self.x/8)
-            celly = (self.y/8)
+        -- Getters
+        get_x = function(self)
+            return self.x
+        end,
 
-            -- Get Sprites around the block
-            cellsprdwn = mget(cellx, celly+1)
+        get_y = function(self)
+            return self.y
+        end,
 
-            -- Check if object collide with obj
-            return fget(cellsprdwn, 0)
+        set_x = function(self, new_x)
+            self.x = new_x
+        end,
+
+        set_y = function(self, new_y)
+            self.y = new_y
         end
     })
 end
--- collision/map.lua
+++ collision/map.lua
@@ -1,94 +1,39 @@
 function _init()
-    score = {
-        time = 0,
-        total = 0
-    }
-
-    -- Sprite # for the blocks
-    blockcolor = {
-        red = 64,
-        orange = 65,
-        yellow = 66,
-        green = 67,
-        pink = 68
-    }
-
+    -- Init Music
     music(0)
-   
-    -- Create Blocks
-    blocks = {}
-    blocks_meta ={
-        size = 0,
-        color = 0
-    }
-    active = false -- Keeps track of activly moving blocks
-    stop = false
-    --add_block(64, 8, blockcolor.pink)
-    --add_block(56, 8, blockcolor.pink)
 
+    -- Init shape
+    init_shape()
 end
 function _update()
+    -- Checks for keypress
     if (btnp(5)) then
         load("debugmenu.p8")
     end
+    
+    -- Update the shape
+    update_shape()
 
-    createshape(1)
-
-    for b in all(blocks) do
-        stop = b:check_collision() or stop
-    end
-
-    if stop then
-        killall()
-        return
-    end
-
-    for b in all(blocks) do
-        b:update()
-    end
-    --update_block()
-
+    -- Create a shape with
+    -- a random type
+    create_shape(rand(1,5))
 end
 
 function _draw()
+    -- Clear Screen
     cls(1)
+
+    -- Update map
     map(0,0)
     
-    print('score', 10, 10, 2)
+    -- Print score
+    -- print('score', 10, 10, 2)
     
-    for b in all(blocks) do
-        b:draw()
-    end
-    --draw_block()
+    draw_shape()
 end
 
 -- EXTRA FUNCTION --
 
-function createshape(type)
-    if active then return end
-
-    if type == 1 then
-        blocks_meta.size = 4
-        blocks_meta.color = blockcolor.pink
-        add_block(48, 8, blocks_meta.color)
-        add_block(56, 8, blocks_meta.color)
-        add_block(64, 8, blocks_meta.color)
-        add_block(72, 8, blocks_meta.color)
-    end
-    active = true
-end
-
-function blockCollision()
-end 
-
-function killall()
-    -- Loop through list
-    for b = #blocks, 1, -1 do
-        blocks[b]:kill()
-    end
-
-    -- Turn off 
-    active = false
-
-    stop = false
+function rand(first, last)
+    return flr(rnd(last)) + first
 end
